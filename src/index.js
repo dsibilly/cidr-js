bb85924d4e28fa1b93c1c1c6374630a943aa4be6
Duane Sibilly <duane@sibilly.com>
2021-12-06 17:57:12 -0500

    Refactor CIDR into JS class
    Additionally, replace old iteration logic with Array iterators (forEach, map, reduce, etc.)

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ const ip2long = require('locutus/php/network/ip2long'),
     long2ip = require('locutus/php/network/long2ip'),
     Calculator = require('ip-subnet-calculator'),
     
-    CIDR = function () {
-        if (!(this instanceof CIDR)) {
-            return new CIDR();
-        }
-    },
-    
     /**
      * Finds the appropriate block or
      * bucket for the given IP to be
      * inserted in. Another way
+     * @param collection
      * @param key
-     * @param ip
+     * @param ipAddress
      */
     block = (collection, key, ipAddress) => {
         if (!collection) {
@@ -27,168 +22,158 @@ const ip2long = require('locutus/php/network/ip2long'),
         }
         
         collection[key].push(ipAddress);
-    };
-
-/**
- * Returns the IP range (start & end)
- * for a given IP/CIDR
- * @param ip
- * @returns {Object}
- */
-CIDR.prototype.range = cidr => {
-    if (!(cidr.indexOf('/') > -1)) {
-        return null;
-    }
-    
-    const range = {},
-        parts = cidr.split('/');
-        
-    if (parts[1] > 32) {
-        return null;
-    }
-    
-    range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
-    range.end = long2ip((ip2long(range.start)) + Math.pow(2, (32 - +parts[1])) - 1);
-    
-    return range;
-};
-
-/**
- * Returns a list of
- * ip values within the range of a
- * given cidr block.
- *
- * @param ip
- * @return {Array}
- */
-CIDR.prototype.list = function(ip) {
-    if (typeof (ip) === 'undefined') {
-        return null;
-    }
-
-    var range = this.range(ip);
-
-    if (!range) {
-        return null;
-    }
-
-    var _ip2long = ip2long;
-    var _long2ip = long2ip;
-    var list = [];
-    var index = 0;
-    var startLong = _ip2long(range.start);
-    var endLong = _ip2long(range.end);
-
-    list[index++] = range.start;
-
-    while((startLong++ < endLong)) {
-        list[index++] = _long2ip(startLong);
-    }
-
-    return list;
-};
-
-var convertAndSort = function(ips) {
-    var len = ips.length;
-    var _ip2long = ip2long;
-    var current = null;
-
-    for (var i=0; i<len; i++) {
-        current = ips[i];
-        if (current) {
-            ips[i] = _ip2long(current);
-        }
-    }
-
-    ips = ips.sort();
-    return ips;
-}
-
-/**
- * Filter the array by grouping
- * IPs where all 32 bits are contiguous
- * 127.0.0.0, 127.0.0.1, 127.0.0.2, etc
- * @returns {Object}
- */
-CIDR.prototype.filter = function(ips) {
-    if (!(ips instanceof Array) || ips.length <= 0) {
-        return null;
-    }
-
-    ips = convertAndSort(ips);
-
-    var key = 0;
-    var cont = true;
-    var len = ips.length;
-    var previous = null;
-    var current = null;
-    var next = null;
-    var results = {};
-
-    if (ips.length === 1) {
-        return block(results, 0, long2ip(ips[0]));
-    }
-
-    for (var i=0; i<len; i++) {
-        current = ips[i];
-        next = ips[i+1];
-        previous = current;
-
-        if (!cont) {
-            key += 1;
-        }
-
-        if (next) {
-            if ((next - current) === 1) {
-                block(results, key, long2ip(current));
-                cont = true;
-            } else {
-                block(results, key, long2ip(current));
-                cont = false;
+    },
+    /**
+     * Converts an array of IP address
+     * strings into long integers (instanceof Number.)
+     * @param ipAddresses
+     */
+    convertAndSort = ipAddresses => {
+        return ipAddresses.map((currentIp, index) => {
+            if (currentIp) {
+                return ip2long(currentIp);
             }
-        } else {
-            if (previous) {
-                if ((current - previous) === 1) {
-                    block(results, key, long2ip(current));
+        }).sort();
+    }
+
+class CIDR {
+    /**
+     * Returns the IP range (start & end)
+     * for a given IP/CIDR
+     * @param cidr
+     * @returns {Object}
+     */
+    range (cidr) {
+        if (!(cidr.indexOf('/') > -1)) {
+            return null;
+        }
+        
+        const range = {},
+            parts = cidr.split('/');
+            
+        if (parts[1] > 32) {
+            return null;
+        }
+        
+        range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
+        range.end = long2ip((ip2long(range.start)) + Math.pow(2, (32 - +parts[1])) - 1);
+        
+        return range;
+    }
+    
+    /**
+     * Returns a list of
+     * ip values within the range of a
+     * given cidr block.
+     *
+     * @param cidr
+     * @return {Array}
+     */
+    list (cidr) {
+        if (typeof cidr === 'undefined') {
+            return null;
+        }
+        
+        const range = this.range(cidr);
+        
+        if (!range) {
+            return null;
+        }
+        
+        const list = [];
+        
+        let startLong = ip2long(range.start),
+            endLong = ip2long(range.end);
+        
+        list.push(range.start);
+        
+        while (startLong++ < endLong) {
+            list.push(long2ip(startLong));
+        }
+        
+        return list;
+    }
+    
+    /**
+     * Filter the array by grouping
+     * IPs where all 32 bits are contiguous
+     * 127.0.0.0, 127.0.0.1, 127.0.0.2, etc
+     * @returns {Object}
+     */
+    filter (ipArray) {
+        if (!(ipArray instanceof Array) || ipArray.length <= 0) {
+            return null;
+        }
+        
+        ipArray = convertAndSort(ipArray);
+        
+        let cont = true,
+            key = 0;
+        
+        if (ipArray.length === 1) {
+            return block({}, 0, long2ip(ipArray[0]));
+        }
+        
+        return ipArray.reduce((results, currentIp, index) => {
+            const nextIp = ipArray[index + 1],
+                previousIp = currentIp;
+            
+            if (!cont) {
+                key += 1;
+            }
+            
+            if (nextIp) {
+                if ((nextIp - currentIp) === 1) {
+                    block(results, key, long2ip(currentIp));
                     cont = true;
                 } else {
-                    block(results, ++key, long2ip(current));
+                    block(results, key, long2ip(currentIp));
+                    cont = false;
+                }
+            } else {
+                if (previousIp) {
+                    if ((currentIp - previousIp) === 1) {
+                        block(results, key, long2ip(currentIp));
+                        cont = true;
+                    } else {
+                        block(results, ++key, long2ip(currentIp));
+                    }
                 }
             }
-        }
+            
+            return results;
+        }, {});
     }
-
-    return results;
-};
-
-/**
- * Returns arrays grouped
- * contiguously.
- *
- * @returns {Array}
- */
-CIDR.prototype.getBlocks = function(ips) {
-    var blocks = this.filter(ips);
-    var results = [];
-
-    for (var i in blocks) {
-        var block = blocks[i];
-
-        if (block.length === 1) {
-            results.push(block[0]);
-            continue;
+    
+    /**
+     * Returns arrays grouped
+     * contiguously.
+     *
+     * @returns {Array}
+     */
+    getBlocks(ipArray) {
+        const blocks = this.filter(ipArray),
+            results = [];
+        
+        for (let i in blocks) {
+            const block = blocks[i];
+            
+            if (block.length === 1) {
+                results.push(block[0]);
+                continue;
+            }
+            
+            const start = block.shift(),
+                end = block.pop(),
+                ranges = Calculator.calculate(start, end);
+            
+            for (let j in ranges) {
+                results.push(ranges[j].ipLowStr + '/' + ranges[j].prefixSize);
+            }
         }
-
-        var start = block.shift();
-        var end = block.pop();
-        var ranges = Calculator.calculate(start, end);
-
-        for (var j in ranges) {
-            results.push(ranges[j].ipLowStr + '/' + ranges[j].prefixSize);
-        }
+        
+        return results;
     }
-
-    return results;
 };
 
 module.exports = CIDR;
